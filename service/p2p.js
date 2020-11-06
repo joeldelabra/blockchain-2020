391051a6839e9817febb65397602d152e1aad87e
joeldelabra <joeldelabra@gmail.com>
2020-11-06 15:52:24 -0700

    Update P2P Broadcasting

--- service/p2p.js
+++ service/p2p.js
@@ -2,6 +2,7 @@ import WebSocket from 'ws';
 
 const {P2P_PORT = 5000, PEERS} = process.env;
 const peers = PEERS ? PEERS.split(',') : [];
+const MESSAGE = {BLOCKS: 'blocks'};
 
 class P2PService {
     constructor(blockchain)
@@ -23,8 +24,23 @@ class P2PService {
     }
 
     onConnection(socket){
+        const { blockchain: {blocks}} = this;
         console.log('[ws:soccket} Conectado');
         this.sockets.push(socket);
+
+        socket.on('message', (message) =>{
+            const {type, value} = JSON.parse(message);
+
+            console.log({type,value});
+        });
+
+        socket.send(JSON.stringify({type: MESSAGE.BLOCKS, value: blocks}));
+    }
+
+    broadcast(type,value){
+        console.log(`[ws: broadcast] ${type}...`);
+        const message = JSON.stringify({type,value});
+        this.sockets.forEach((socket)=> socket.send(message));
     }
 }
 
